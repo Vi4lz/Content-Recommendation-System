cce8b90259c53d8de75ec340c6d2cd64281df545
Sarunas Tumasonis <tumasonissarunas@gmail.com>
2025-05-05 10:11:12 +0300

    Removed .venv/ from tracking and updated .gitignore

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -7,11 +7,15 @@ def load_all_csv_files(data_dir='../data'):
     and returns a dictionary, where:
     - key: file name without the .csv extension
     - value: a specific pandas DataFrame
-    :param data_dir:
-    :return: datasets
+    Returns:
+        datasets from the given directory
     """
 
     datasets = {}
+
+    if not os.path.exists(data_dir):
+        print(f"Directory {data_dir} does not exist.")
+        return {}
 
     for file_name in os.listdir(data_dir):
         if file_name.endswith('.csv'):
@@ -20,8 +24,24 @@ def load_all_csv_files(data_dir='../data'):
                 df = pd.read_csv(file_path)
                 key_name = file_name.replace('.csv', '')
                 datasets[key_name] = df
-                print(f"Loaded: {file_name} ({df.shape[0]} row., {df.shape[1]} column.)")
+                print(f"Loaded: {file_name} ({df.shape[0]} rows., {df.shape[1]} columns.)")
             except Exception as e:
-                print(f"Error reading {file_name}: {e}")
+                print(f"Error reading {file_name} ({file_path}): {e}")
 
     return datasets
+
+
+def merge_ratings_with_movies(ratings_df, movies_df):
+    """
+    Merge the ratings DataFrame with the movies DataFrame using the 'movieId' column.
+    Returns:
+        pd.DataFrame: A merged DataFrame that combines rating data with corresponding movie details.
+    """
+    try:
+        merged = pd.merge(ratings_df, movies_df, on='movieId', how='left')
+        print(f"Merged table: {merged.shape[0]} rows, {merged.shape[1]} columns.")
+        return merged
+    except Exception as e:
+        print(f"Error while merging the DataFrames: {e}")
+        return None
+
--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
-from data_preprocessing import load_all_csv_files
+from data_preprocessing import load_all_csv_files, merge_ratings_with_movies
 
 datasets = load_all_csv_files('../data')
 
 movies = datasets.get('movies')
 ratings = datasets.get('ratings')
 
-if movies is not None:
-    print("\n Movies:")
-    print(movies.head())
+# if movies is not None:
+#     print("\n Movies:")
+#     print(movies.head())
+#
+# if ratings is not None:
+#     print("\n Ratings:")
+#     print(ratings.head())
 
-if ratings is not None:
-    print("\n Ratings:")
-    print(ratings.head())
+
+if movies is not None and ratings is not None:
+    merged_data = merge_ratings_with_movies(ratings, movies)
+    print(merged_data.head().to_string())
